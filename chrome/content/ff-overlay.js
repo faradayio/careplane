33232e63fbbf36f34d52557e964f9016ea3e3eec
Andy Rossmeissl <andy@rossmeissl.net>
2011-03-14 15:17:57 -0400

    Housecleaning and set default preferences

--- chrome/content/ff-overlay.js
+++ chrome/content/ff-overlay.js
@@ -1,11 +1,1 @@
-careplane.onFirefoxLoad = function(event) {
-  document.getElementById("contentAreaContextMenu")
-          .addEventListener("popupshowing", function (e){ careplane.showFirefoxContextMenu(e); }, false);
-};
 
-careplane.showFirefoxContextMenu = function(event) {
-  // show or hide the menuitem based on what the context menu is on
-  document.getElementById("context-careplane").hidden = gContextMenu.onImage;
-};
-
-window.addEventListener("load", function () { careplane.onFirefoxLoad(); }, false);
--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,6 +1,5 @@
-pref("extensions.careplane.boolpref", false);
-pref("extensions.careplane.intpref", 0);
-pref("extensions.careplane.stringpref", "A string");
+pref("extensions.careplane.kayakpref", true);
+pref("extensions.careplane.orbitzpref", true);
 
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
 pref("extensions.careplane@brighterplanet.com.description", "chrome://careplane/locale/overlay.properties");
