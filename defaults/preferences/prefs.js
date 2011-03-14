97c116856e08d9e50901a30b2e40d5149b888d90
Andy Rossmeissl <andy@rossmeissl.net>
2011-03-14 14:41:54 -0400

    Add-on stub from https://addons.mozilla.org/en-US/developers/tools/builder

---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,6 @@
+pref("extensions.careplane.boolpref", false);
+pref("extensions.careplane.intpref", 0);
+pref("extensions.careplane.stringpref", "A string");
+
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.careplane@brighterplanet.com.description", "chrome://careplane/locale/overlay.properties");
