a521dc716880a83dd67a80ccb016c9cff3eeeb91
Derek Kastner <dkastner@gmail.com>
2011-03-23 21:13:35 -0400

    Initial Orbitz driver with all tests passing

--- chrome/content/drivers/orbitz.js
+++ chrome/content/drivers/orbitz.js
@@ -1,6 +1,96 @@
 var Orbitz = {
-    name: 'Orbitz',
-    searchPattern: 'orbitz.com/App/ViewFlightSearchResults'
+  name: 'Orbitz',
+  searchPattern: 'orbitz.com/App/ViewFlightSearchResults',
+
+  insertAttribution: function() {
+    // In the sidebar
+    var parentElement = top.window.document.getElementById('matrix');
+    // var referenceElement = top.window.document.getElementById('airResultMatrix');
+    Careplane.insertBadge(parentElement, null, null);
+  },
+
+  scoreFlights: function() {
+    var airResults = top.window.document.getElementsByClassName('airResults')[0];
+    var results = airResults.getElementsByClassName('result');
+
+    for(var i = 0; i < results.length; i++) {
+      var result = results[i];
+      var scoreKeeper = new OrbitzScoreKeeper(result);
+      scoreKeeper.officiate();
+    }
+  }
 }
 
 
+// OrbitzScoreKeeper
+
+OrbitzScoreKeeper = function(resultNode) {
+  this.resultNode = resultNode;
+  this.legs = this.resultNode.getElementsByClassName('resultLeg');
+  this.completeCount = 0;
+  this.totalEmissions = 0;
+
+  this.totalFootprintP = top.window.document.createElement('p');
+  this.totalFootprintP.setAttribute('class', 'careplane-footprint total-footprint');
+  this.totalFootprintP.style.color = '#aaa';
+  this.totalFootprintP.innerHTML = 'Total emissions: <em>Loading...</em>';
+  this.resultNode.appendChild(this.totalFootprintP);
+};
+
+OrbitzScoreKeeper.prototype.officiate = function() {
+  for(var i = 0; i < this.legs.length; i++) {
+    var leg = this.legs[i];
+    var flight = OrbitzFlight.parse(leg);
+    flight.emissionEstimate(this.onEmissionsSuccess(leg, this));
+  }
+};
+
+OrbitzScoreKeeper.prototype.onEmissionsSuccess = function(legElement, scoreKeeper) {
+  return function(emission) {
+    var footprintP = window.document.createElement('p');
+    footprintP.setAttribute('class', 'careplane-footprint');
+    footprintP.style.color = '#aaa';
+    footprintP.innerHTML = Careplane.numberWithDelimiter(emission) + 'kg CO2e';
+
+    legElement.appendChild(footprintP)
+    scoreKeeper.completeCount++;
+    scoreKeeper.totalEmissions += emission;
+    if(scoreKeeper.completeCount == scoreKeeper.legs.length) {
+      scoreKeeper.onEmissionsFinished();
+    }
+  };
+};
+
+OrbitzScoreKeeper.prototype.onEmissionsFinished = function() {
+  this.totalFootprintP.innerHTML = 'Total emissions: ' + Careplane.numberWithDelimiter(this.totalEmissions) + 'kg CO2e';
+};
+
+
+// OrbitzFlight
+
+OrbitzFlight = function(origin, destination, airline, aircraft) {
+  this.origin = origin;
+  this.destination = destination;
+  this.airline = airline;
+  this.aircraft = aircraft;
+};
+OrbitzFlight.prototype = Flight.prototype;
+
+OrbitzFlight.parse = function(legElement) {
+  var origin, destination, airline, aircraft;
+
+  var airCodes = legElement.getElementsByClassName('airCode');
+  origin = airCodes[0].getElementsByTagName('span')[0].innerText;
+  destination = airCodes[1].getElementsByTagName('span')[0].innerText;
+
+  var legTable = legElement.getElementsByTagName('table')[0];
+  airline = legTable.getElementsByClassName('col3')[0].innerText;
+  airline = airline.match(/([^\d]+)/)[1];
+  airline = airline.replace(/\s+$/,'');
+
+  var extraInfo = legElement.getElementsByClassName('legExtraInfo')[0];
+  var aircraftLi = extraInfo.getElementsByTagName('li')[2];
+  aircraft = aircraftLi.innerText;
+
+  return new OrbitzFlight(origin, destination, airline, aircraft);
+};
---
+++ spec/javascripts/drivers/orbitz_spec.js
@@ -0,0 +1,78 @@
+describe('Orbitz', function() {
+  describe('.insertAttribution', function() {
+    beforeEach(function() {
+      loadFixtures('orbitz_dtw_sfo.html');
+      Orbitz.insertAttribution();
+    });
+    it('inserts a badge in the top area', function() {
+      expect($('div#trogdor')).toContain('script[src$="badge.js"]');
+    });
+  });
+  describe('.scoreFlights', function() {
+    it('asks the OrbitzScoreKeeper to officiate each result', function() {
+      loadFixtures('orbitz_dtw_sfo.html');
+      Careplane.fetch = function(url, callback) {
+        callback(JSON.stringify({ emission: 512.0 }));
+      }
+      Orbitz.scoreFlights();
+
+      $('div.result').each(function(i, result) {
+        expect($(result)).toContain('p.total-footprint');
+        expect($(result).children('p.total-footprint').get(0).innerText).
+          toMatch(/Total emissions: [\d,]+kg CO2e/);
+      });
+    });
+  });
+});
+
+describe('OrbitzScoreKeeper', function() {
+  var result, keeper;
+
+  beforeEach(function() {
+    loadFixtures('orbitz_dtw_sfo_result.html');
+    result = $('.result').get(0);
+    keeper = new OrbitzScoreKeeper(result);
+  });
+
+  describe('#officiate', function() {
+    it('parses each flight and totals emissions', function() {
+      Careplane.fetch = function(url, callback) {
+        callback(JSON.stringify({ emission: 123.0 }));
+      }
+      keeper.officiate();
+      expect($('.resultLeg .careplane-footprint').first()).toHaveText('123kg CO2e');
+      expect($('.resultLeg .careplane-footprint').last()).toHaveText('123kg CO2e');
+      expect($('.result .total-footprint')).toHaveText('Total emissions: 246kg CO2e');
+    });
+  });
+  describe('#onEmissionsSuccess', function() {
+    it('updates the specified resultLeg node with emissions', function() {
+      var leg = $('.resultLeg').get(0);
+      var func = keeper.onEmissionsSuccess(leg, keeper);
+      func(123.0);
+      expect($('.resultLeg p.careplane-footprint')).toHaveText('123kg CO2e');
+    });
+    it('updates the total emissions result when all emissions are finished', function() {
+      var func = keeper.onEmissionsSuccess($('.resultLeg').get(0), keeper);
+      func(123.0);
+      func = keeper.onEmissionsSuccess($('.resultLeg').get(1), keeper);
+      func(123.0);
+      expect($('.result .total-footprint')).toHaveText('Total emissions: 246kg CO2e');
+    });
+  });
+});
+
+describe('OrbitzFlight', function() {
+  describe('.parse', function() {
+    it('parses an Orbitz leg', function() {
+      loadFixtures('orbitz_dtw_sfo_result.html');
+      var node = $('.resultLeg').get(0);
+
+      var flight = OrbitzFlight.parse(node);
+      expect(flight.origin).toBe('DTW');
+      expect(flight.destination).toBe('SFO');
+      expect(flight.airline).toBe('Delta Air Lines');
+      expect(flight.aircraft).toBe('Boeing 737');
+    });
+  });
+});
