3ef42986753e45e156a50c1ffdb311a1a83440fb
Derek Kastner <dkastner@gmail.com>
2011-03-30 16:23:27 -0400

    Set appropriate trips and segments_per_trip parameters

--- chrome/content/models/flight.js
+++ chrome/content/models/flight.js
@@ -1,8 +1,10 @@
 var Flight = function(origin, destination, airline, aircraft) {
-    this.origin = origin;
-    this.destination = destination;
-    this.airline = airline;
-    this.aircraft = aircraft;
+  this.origin = origin;
+  this.destination = destination;
+  this.airline = airline;
+  this.aircraft = aircraft;
+  this.segments_per_trip = 1;
+  this.trips = 1;
 }
 
 Flight.prototype.inspect = function() {
@@ -10,7 +12,7 @@ Flight.prototype.inspect = function() {
 };
 
 Flight.prototype.emissionEstimate = function(callback, identifier, totalSegments) {
-  var url = encodeURI('http://carbon.brighterplanet.com/flights.json?key=' + Careplane.brighterPlanetKey + '&origin_airport=' + this.origin + '&destination_airport=' + this.destination + '&airline=' + this.airline + '&aircraft=' + this.aircraft);
+  var url = encodeURI('http://carbon.brighterplanet.com/flights.json?key=' + Careplane.brighterPlanetKey + '&origin_airport=' + this.origin + '&destination_airport=' + this.destination + '&airline=' + this.airline + '&aircraft=' + this.aircraft + '&segments_per_trip=1&trips=1');
   Careplane.fetch(url, function(response) {
       var json = JSON.parse(response);
       callback(json.emission, identifier, totalSegments);
