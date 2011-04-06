2a677f086d4271bd336c29e37e39b06a976834c2
Derek Kastner <dkastner@gmail.com>
2011-04-06 14:04:34 -0400

    Made trip parsing more testable

--- chrome/content/models/Trip.js
+++ chrome/content/models/Trip.js
@@ -1,10 +1,29 @@
 Trip = function() {}
 
+Trip.prototype.addDetails = function(html) {
+  if(!this.debugDiv) {
+    this.debugDiv = Careplane.webDoc.createElement('div');
+    this.debugDiv.style.display = 'none';
+    this.footprintParagraph.parentNode.appendChild(this.debugDiv);
+  }
+
+  this.debugDiv.innerHTML += html;
+};
+
+Trip.prototype.reportFootprint = function() {
+  this.footprintParagraph.innerHTML = Careplane.formatFootprint(this.totalFootprint);
+};
+
+Trip.prototype.reportFlightMethodology = function(methodologyUrl, flight) {
+  this.addDetails('<a href="' + methodologyUrl + '">Methodology for ' + flight.origin + '-' + flight.destination + '</a>');
+};
+
 Trip.prototype.onFlightEmissionsComplete = function(onTripEmissionsComplete) {
   var trip = this;
-  return function(footprint) {
-    trip.totalFootprint += footprint;
-    trip.footprintParagraph.innerHTML = Careplane.formatFootprint(trip.totalFootprint);
+  return function(cm1Response, flight) {
+    trip.totalFootprint += cm1Response.emission;
+    trip.reportFootprint();
+    trip.reportFlightMethodology(cm1Response.methodology, flight);
     trip.completedFlightCount++;
     if(onTripEmissionsComplete && trip.isDone())
       onTripEmissionsComplete();
--- spec/javascripts/models/TripSpec.js
+++ spec/javascripts/models/TripSpec.js
@@ -1,7 +1,20 @@
 describe('Trip', function() {
-  describe('#rate', function() {
-    var trip;
+  var trip, onTripEmissionsComplete, onFlightEmissionsComplete, p;
+  beforeEach(function() {
+    var parent = document.createElement('div');
+    p = document.createElement('p');
+    parent.appendChild(p);
+    trip = new Trip();
+    trip.flights = function() { return [1,2,3]; };
+    trip.footprintParagraph = p;
+    trip.totalFootprint = 0;
+    trip.completedFlightCount = 0;
+    onTripEmissionsComplete = jasmine.createSpy('onTripEmissionsComplete');
+    onFlightEmissionsComplete = trip.onFlightEmissionsComplete(onTripEmissionsComplete);
+  });
 
+
+  describe('#rate', function() {
     beforeEach(function() {
       trip = new Trip();
       trip.footprintParagraph = window.document.createElement('p');
@@ -22,28 +35,27 @@ describe('Trip', function() {
   });
 
   describe('#onFlightEmissionsComplete', function() {
-    var trip, onTripEmissionsComplete, onFlightEmissionsComplete, p;
-
-    beforeEach(function() {
-      p = document.createElement('p');
-      trip = new Trip();
-      trip.flights = function() { return [1,2,3]; };
-      trip.footprintParagraph = p;
-      trip.totalFootprint = 0;
-      trip.completedFlightCount = 0;
-      onTripEmissionsComplete = jasmine.createSpy('onTripEmissionsComplete');
-      onFlightEmissionsComplete = trip.onFlightEmissionsComplete(onTripEmissionsComplete);
-    });
-
     it('updates the footprint paragraph with the latest total', function() {
-      onFlightEmissionsComplete(2714.8);
+      onFlightEmissionsComplete({ emission: 2714.8 }, {});
       expect(p.innerHTML).toMatch(/5,972.6/);
     });
     it('executes the onTripEmissionsComplete function when all flights are ready', function() {
-      onFlightEmissionsComplete(1);
-      onFlightEmissionsComplete(2);
-      onFlightEmissionsComplete(3);
+      onFlightEmissionsComplete({ emission: 1 }, {});
+      onFlightEmissionsComplete({ emission: 2 }, {});
+      onFlightEmissionsComplete({ emission: 3 }, {});
       expect(onTripEmissionsComplete).toHaveBeenCalled();
+    });
+  });
+
+  describe('#addDetails', function() {
+    it('creates a details div if none exists', function() {
+      trip.addDetails('hi');
+      expect($(trip.footprintParagraph.nextSibling)).toHaveText('hi');
+    });
+    it('adds details to an existing details div', function() {
+      trip.addDetails('hi');
+      trip.addDetails(' there');
+      expect($(trip.footprintParagraph.nextSibling)).toHaveText('hi there');
     });
   });
 });
