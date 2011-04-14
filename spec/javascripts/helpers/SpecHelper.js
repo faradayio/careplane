8de6e1f1fa1a0ab2943e53e689ca707e5c40756f
Derek Kastner <dkastner@gmail.com>
2011-04-14 14:11:14 -0400

    Add shared example groups, a la RSpec

--- spec/javascripts/helpers/SpecHelper.js
+++ spec/javascripts/helpers/SpecHelper.js
@@ -7,3 +7,27 @@ beforeEach(function() {
   Careplane.webDoc = window.document;
   Careplane.log = jasmine.log;
 });
+
+jasmine.exampleGroups = {};
+
+jasmine.Env.prototype.itBehavesLikeA = function(subject) {
+  var group = jasmine.exampleGroups[subject];
+  if(group) {
+    group.call(this);
+  } else {
+    jasmine.log('Missing example group "' + subject + '"');
+  }
+}
+
+jasmine.Env.prototype.sharedExamplesFor = function(subject, suite) {
+  jasmine.exampleGroups[subject] = suite;
+}
+
+var itBehavesLikeA = function(subject) {
+  return jasmine.getEnv().itBehavesLikeA(subject);
+}
+var itBehavesLikeAn = itBehavesLikeA;
+
+var sharedExamplesFor = function(subject, suite) {
+  return jasmine.getEnv().sharedExamplesFor(subject, suite);
+}
