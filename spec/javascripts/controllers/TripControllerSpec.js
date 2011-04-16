f5a4eff2226a7de78e7d86b2b6add4cb5bf1028f
Derek Kastner <dkastner@gmail.com>
2011-04-15 21:49:29 -0400

    Added TripController to manifest

---
+++ spec/javascripts/controllers/TripControllerSpec.js
@@ -0,0 +1,33 @@
+describe('TripController', function() {
+  var trip, footprintView, infoView;
+  beforeEach(function() {
+    loadFixtures('kayak_dtw_sfo_direct_flight.html');
+    trip = new KayakTrip($('.flightresult').get(0));
+    spyOn(trip.infoView(), 'show');
+    spyOn(trip.infoView(), 'hide');
+  });
+
+  describe('TripFootprintView#mouseover', function() {
+    it('shows the TripInfoView', function() {
+      var mouseover = TripController.events.tripFootprintHoverIn(trip);
+      mouseover();
+      expect(trip.infoView().show).toHaveBeenCalled();
+    });
+  });
+  describe('TripFootprintView#mouseout', function() {
+    it('hides the TripInfoView', function() {
+      var mouseout = TripController.events.tripFootprintHoverOut(trip);
+      mouseout();
+      expect(trip.infoView().hide).toHaveBeenCalled();
+    });
+  });
+
+  describe('#init', function() {
+    it('does not explode', function() {
+      var controller = new TripController(trip);
+      expect(function() {
+        controller.init();
+      }).not.toThrow();
+    });
+  });
+});
