d7f3625c3a0b66f06669a1e3005323c3c5bfac32
Derek Kastner <dkastner@gmail.com>
2011-04-26 09:59:21 -0400

    Don't hide hipmunk info on init

--- src/views/Hipmunk/HipmunkTripInfoView.js
+++ src/views/Hipmunk/HipmunkTripInfoView.js
@@ -8,6 +8,10 @@ HipmunkTripInfoView.prototype.target = function() {
   return this.tripElement.getElementsByClassName('careplane-info')[0];
 };
 
+HipmunkTripInfoView.prototype.init = function() {
+  this.tripElement.innerHTML += this.content();
+};
+
 HipmunkTripInfoView.prototype.content = function() {
   return "\
     <div class=\"leg careplane-info\">\
