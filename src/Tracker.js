c61693950563838da769d30592e965c2389dc34a
Derek Kastner <dkastner@gmail.com>
2011-05-11 18:58:46 -0400

    SafariTracker now works with Safari

--- src/Tracker.js
+++ src/Tracker.js
@@ -26,11 +26,11 @@ SafariTracker = {
     safari.self.tab.dispatchMessage('tracker.firstRun');
   },
   search: function(site, origin, destination, averageCo2) {
-    chrome.extension.sendRequest({ action: 'tracker.search',
-      site: site, origin: origin, destination: destination, averageCo2: averageCo2 });
+    safari.self.tab.dispatchMessage('tracker.search',
+        { site: site, origin: origin, destination: destination, averageCo2: averageCo2 });
   },
   purchase: function(origin, destination, cost, minCost, co2, averageCo2) {
-    chrome.extension.sendRequest({ action: 'tracker.purchase',
+    safari.self.tab.dispatchMessage('tracker.purchase', {
       origin: origin, destination: destination, cost: cost, minCost: minCost, co2: co2, averageCo2: averageCo2 });
   }
 };
