037e0e26db10c4bf5b22399aad2774c9a0f22c9d
Derek Kastner <dkastner@gmail.com>
2011-06-22 15:14:31 -0400

    Fixed KayakFlight fixture/specs

--- spec/javascripts/drivers/Kayak/KayakFlightSpec.js
+++ spec/javascripts/drivers/Kayak/KayakFlightSpec.js
@@ -3,7 +3,7 @@ describe('KayakFlight', function() {
     var flights;
     beforeEach(function() {
       loadFixtures('kayak_dtw_sfo_flight.html');
-      flights = KayakFlight.parse($('.inlineFlightDetailsLeg tr', document));
+      flights = KayakFlight.parse($('.inlineflightitinerarylegs tr'));
     });
 
     it('returns an array of flights', function() {
@@ -11,25 +11,25 @@ describe('KayakFlight', function() {
     });
     
     it('parses airline, origin, destination, and aircraft', function() {
-      expect(flights[0].airline).toBe('AirTran &mdash; Flight 143');
+      expect(flights[0].airline).toMatch(/AirTran/);
       expect(flights[0].origin).toBe('DTW');
       expect(flights[0].destination).toBe('ATL');
-      expect(flights[0].aircraft).toBe('Boeing 717');
+      expect(flights[0].aircraft).toBe('Boeing 737-700');
 
-      expect(flights[1].airline).toBe('AirTran &mdash; Flight 321');
+      expect(flights[1].airline).toMatch(/AirTran/);
       expect(flights[1].origin).toBe('ATL');
       expect(flights[1].destination).toBe('SFO');
       expect(flights[1].aircraft).toBe('Boeing 737-700');
 
-      expect(flights[2].airline).toBe('AirTran &mdash; Flight 328');
+      expect(flights[2].airline).toMatch(/AirTran/);
       expect(flights[2].origin).toBe('SFO');
       expect(flights[2].destination).toBe('ATL');
       expect(flights[2].aircraft).toBe('Boeing 737-700');
 
-      expect(flights[3].airline).toBe('AirTran &mdash; Flight 140');
+      expect(flights[3].airline).toMatch(/AirTran/);
       expect(flights[3].origin).toBe('ATL');
       expect(flights[3].destination).toBe('DTW');
-      expect(flights[3].aircraft).toBe('Boeing 717');
+      expect(flights[3].aircraft).toBe('Boeing 737-700');
     });
   });
 
@@ -37,12 +37,12 @@ describe('KayakFlight', function() {
     var extra;
     beforeEach(function() {
       loadFixtures('kayak_dtw_sfo_flight.html');
-      extra = $('.inlineFlightDetailsLeg td.extra div').text();
+      extra = $('.inlineflightitinerarylegs td.extra').text();
     });
 
     it('returns the name of the aircraft', function() {
       var aircraft = KayakFlight.parseAircraft(extra);
-      expect(aircraft).toMatch('Boeing 717');
+      expect(aircraft).toMatch('Boeing 737-700');
     });
     it('filters out aircraft details', function() {
       var aircraft = KayakFlight.parseAircraft('hi|Embraer (Winglets) (Narrow-body)|man');
