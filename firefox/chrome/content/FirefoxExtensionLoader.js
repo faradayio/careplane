32060a141e6c95049c3f5911d20e74a80b117a5e
Derek Kastner <dkastner@gmail.com>
2011-06-24 17:12:40 -0400

    Skeleton Jetpack Addon for FF5

--- firefox/chrome/content/FirefoxExtensionLoader.js
+++
@@ -1,6 +0,0 @@
-FirefoxExtensionLoader = ExtensionLoader;
-
-FirefoxExtensionLoader.load = function(ev) {
-  var extension = new FirefoxExtension(ev.originalTarget);
-  extension.loadDriver(FirefoxExtensionLoader.driverLoaded(extension));
-};
