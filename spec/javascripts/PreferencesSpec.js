0a987a13733c7749f7f36a3060755c9b25e59546
Derek Kastner <dkastner@gmail.com>
2011-06-29 10:10:23 -0400

    Fixed preference specs

--- spec/javascripts/PreferencesSpec.js
+++ spec/javascripts/PreferencesSpec.js
@@ -2,6 +2,7 @@ describe('Preferences', function() {
   var preferences;
   beforeEach(function() {
     preferences = new Preferences();
+    preferences.callbacks = [];
   });
 
   describe('#get', function() {
@@ -17,37 +18,25 @@ describe('Preferences', function() {
       preferences.get('foo', function(val) { result = val; });
       expect(result).toBeNull();
     });
-    it('stores a default value for an undefined preference if a default is set', function() {
-      preferences.nativeGet = function(key, callback) { callback(null); };
-      var result;
-      preferences.get('foo', function(val) { result = val; }, 'cool');
-      expect(result).toBe('cool');
-    });
   });
   describe('#getBoolean', function() {
     it('provides an existing preference to a given callback', function() {
-      preferences.nativeGetBoolean = function(key, callback) { callback(true); };
+      preferences.nativeGet = function(key, callback) { callback(true); };
       var result;
       preferences.getBoolean('foo', function(val) { result = val; }, 'lame');
       expect(result).toBeTruthy();
     });
     it('provides an false preference to a given callback', function() {
-      preferences.nativeGetBoolean = function(key, callback) { callback(false); };
+      preferences.nativeGet = function(key, callback) { callback(false); };
       var result;
       preferences.getBoolean('foo', function(val) { result = val; }, 'lame');
       expect(result).toBeFalsy();
     });
-    it('sends null to the callback if the preference is not set', function() {
-      preferences.nativeGetBoolean = function(key, callback) { return callback(null); };
+    it('sends false to the callback if the preference is not set', function() {
+      preferences.nativeGet = function(key, callback) { return callback(null); };
       var result;
       preferences.getBoolean('foo', function(val) { result = val; });
-      expect(result).toBeNull()
-    });
-    it('stores a default value for an undefined preference if a default is set', function() {
-      preferences.nativeGetBoolean = function(key, callback) { callback(null); };
-      var result;
-      preferences.getBoolean('foo', function(val) { result = val; }, true);
-      expect(result).toBeTruthy();
+      expect(result).toBeFalsy()
     });
   });
 });
