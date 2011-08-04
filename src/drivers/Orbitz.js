a2024079203958e999108859da9a110279ce51e8
Derek Kastner <dkastner@gmail.com>
2011-08-04 10:59:31 -0400

    Make Orbitz regex liberal enough for Firefox

--- src/drivers/Orbitz.js
+++ src/drivers/Orbitz.js
@@ -12,7 +12,7 @@ Orbitz.prototype = new Driver();
 
 Orbitz.driverName = 'Orbitz';
 
-Orbitz.monitorURL = /orbitz\.com\/App\/ViewFlightSearchResults/;
+Orbitz.monitorURL = /.*orbitz\.com\/App\/ViewFlightSearchResults.*/;
 Orbitz.monitorExcludeURL = /track\.pubmatic\.com/;
 
 Orbitz.prototype.isPollingEnabled = function() {
