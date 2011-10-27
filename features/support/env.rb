b52081ec24693af0ff2317c93f2f6afce2cab1d6
Derek Kastner <dkastner@gmail.com>
2011-10-27 11:49:24 -0400

    Cull gems

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,14 +7,7 @@ require 'headless'
 
 require 'capybara'
 require 'capybara/cucumber'
-require 'capybara/driver/selenium_driver'
-
 require 'capybara/session'
-
-require 'selenium/webdriver'
-
-require 'capybara/mechanize'
-require 'capybara/mechanize/cucumber'
 require 'capybara-webkit'
 
 ENV['BROWSER'] ||= 'firefox'
@@ -26,26 +19,7 @@ ENV['PROFILE'] ||= Dir.glob('/Users/dkastner/Library/Application Support/Firefox
 # prefer to use XPath just remove this line and adjust any selectors in your
 # steps to use the XPath syntax.
 Capybara.default_selector = :css
-Capybara.register_driver :selenium do |app|
-  profile = Selenium::WebDriver::Firefox::Profile.new(ENV['PROFILE'])
-  client = Selenium::WebDriver::Remote::Http::Default.new
-  client.timeout = 300
-  Capybara::Driver::Selenium.new(app, :profile => 'Selenium',
-                                 :browser => ENV['BROWSER'].to_sym,
-                                 :http_client => client)
-end
-
 Capybara.run_server = false
-
-case ENV['CAPY']
-when 'selenium' then
-  Capybara.javascript_driver = :selenium
-  Capybara.current_driver = :selenium
-when 'webkit' then
-  Capybara.javascript_driver = :webkit
-  Capybara.current_driver = :mechanize
-end
-
 Capybara.default_wait_time = 3600
 
 Dir.glob('*.html') { |f| FileUtils.rm_f f }
