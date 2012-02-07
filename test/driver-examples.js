63c8dd4ff584c562f4e87d84bcd889517e2bf10b
Derek Kastner <dkastner@gmail.com>
2012-02-07 13:42:20 -0500

    Get rid of non-polling drivers

--- test/driver-examples.js
+++ test/driver-examples.js
@@ -2,13 +2,12 @@ var helper = require('./helper'),
     assert = helper.assert,
     sinon = helper.sinon,
     plugin = helper.plugin,
-    Careplane = plugin.require('./careplane'),
-    TestExtension = plugin.require('./browser/test/test-extension');
+    Careplane = plugin.require('./careplane');
 
 module.exports.pollingDriver = function(driverClass) {
   return {
-    'Load': {
-      'polls when using a non-testing extension': function() {
+    '#load': {
+      'polls the page for updates': function() {
         var extension = new Careplane();
         extension.doc = {
           location: {
@@ -20,52 +19,6 @@ module.exports.pollingDriver = function(driverClass) {
         driver.prepare = function() {};
         driver.load();
         assert(driver.events.loadPoller.called);
-      },
-      'does not poll when using a testing extension': function() {
-        var extension = new TestExtension();
-        extension.doc = {
-          location: {
-            href: ''
-          }
-        };
-        var driver = new driverClass(extension);
-        sinon.spy(driver.atc, 'clear');
-        driver.prepare = function() {};
-        driver.load();
-        assert(driver.atc.clear.called);
-      }
-    }
-  };
-};
-
-module.exports.nonPollingDriver = function(driverClass) {
-  return {
-    'Load': {
-      'does not poll when using a non-testing extension': function() {
-        var extension = new Careplane();
-        extension.doc = {
-          location: {
-            href: ''
-          }
-        };
-        var driver = new driverClass(extension);
-        sinon.spy(driver.atc, 'clear');
-        driver.prepare = function() {};
-        driver.load();
-        assert(driver.atc.clear.called);
-      },
-      'does not poll when using a testing extension': function() {
-        var extension = new TestExtension();
-        extension.doc = {
-          location: {
-            href: ''
-          }
-        };
-        var driver = new driverClass(extension);
-        sinon.spy(driver.atc, 'clear');
-        driver.prepare = function() {};
-        driver.load();
-        assert(driver.atc.clear.called);
       }
     }
   };
