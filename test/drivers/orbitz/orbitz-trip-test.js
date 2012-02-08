00cb4bbaa48bfa84412777e6411bd95731d31409
Derek Kastner <dkastner@gmail.com>
2012-02-07 21:28:17 -0500

    Passing OrbitzTrip test for new Orbitz

--- test/drivers/orbitz/orbitz-trip-test.js
+++ test/drivers/orbitz/orbitz-trip-test.js
@@ -1,17 +1,24 @@
-var helper = require('./helper'),
-    vows = helper.vows,
-    assert = helper.assert,
-    sinon = helper.sinon;
-require('../../trip-examples');
+var test = require('../../helper'),
+    vows = test.vows;
 
-vows.describe('OrbitzTrip').addBatch({
-  var JasmineExtension = require('browser/jasmine/jasmine-extension');
-  var OrbitzTrip = require('drivers/orbitz/orbitz-trip');
+var tripExamples = require('../../trip-examples');
 
-  beforeEach(function() {
-    loadFixtures('orbitz_dtw_sfo_result.html');
-    this.trip = new OrbitzTrip(0, $('.result').get(0));
-  });
+var OrbitzTrip = test.plugin.require('./drivers/orbitz/orbitz-trip');
 
-  itBehavesLikeA('Trip');
+var http = require('http'),
+    fakeweb = require('fakeweb'),
+    fs = require('fs'),
+    path = require('path');
+
+var orbitzFlightDetails = fs.readFileSync(path.resolve(__dirname, '../../fixtures/orbitz_dtw_sfo_flight_details.html'));
+http.register_intercept({
+  uri: /.*/,
+  host: 'www.orbitz.com',
+  body: orbitzFlightDetails
 });
+
+vows.describe('OrbitzTrip').addBatch(
+  tripExamples.trip('orbitz_dtw_sfo_result.html', function($) {
+    return new OrbitzTrip(0, $('.airResultsCard').get(0));
+  })
+).export(module);
