5816b328fc92501de402c7d1da3c88ec5ab601f7
Derek Kastner <dkastner@gmail.com>
2012-03-13 17:20:49 -0400

    Test to try and ferret out hanging problem

--- test/drivers/orbitz/orbitz-trip-test.js
+++ test/drivers/orbitz/orbitz-trip-test.js
@@ -1,9 +1,10 @@
-var test = require('../../helper'),
-    vows = test.vows;
+var helper = require('../../helper'),
+    vows = helper.vows,
+    assert = helper.assert;
 
 var tripExamples = require('../../trip-examples');
 
-var OrbitzTrip = test.plugin.require('./drivers/orbitz/orbitz-trip');
+var OrbitzTrip = helper.plugin.require('./drivers/orbitz/orbitz-trip');
 
 var http = require('http'),
     fakeweb = require('fakeweb'),
@@ -21,4 +22,15 @@ vows.describe('OrbitzTrip').addBatch(
   tripExamples('orbitz_dtw_sfo_result.html', function($) {
     return new OrbitzTrip(0, $, $('.airResultsCard').get(0));
   })
-).export(module);
+).addBatch({
+  '#loadFlightsFromDetails': {
+    'loads details for a United flight': function() {
+      var $ = helper.qweryFixture('orbitz_dtw_sfo_united.html');
+      var result = $('.airResultsCard').get(0);
+      var trip = new OrbitzTrip(0, $, result);
+      trip.flights = [];
+      trip.loadFlightsFromDetails($(result), function() {});
+      assert.equal(trip.flights.length, 4);
+    } 
+  }
+}).export(module);
