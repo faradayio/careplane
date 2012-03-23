b094f0020bd5a9ca3116ccdaf5a84ede398870f6
Derek Kastner <dkastner@gmail.com>
2012-03-23 16:09:17 -0400

    Fix for hipmunk tab switching

--- test/drivers/hipmunk/hipmunk-leg-test.js
+++ test/drivers/hipmunk/hipmunk-leg-test.js
@@ -11,23 +11,22 @@ var HipmunkLeg = plugin.require('./drivers/hipmunk/hipmunk-leg'),
     HipmunkFlight = plugin.require('./drivers/hipmunk/hipmunk-flight'),
     HipmunkRailTrip = plugin.require('./drivers/hipmunk/hipmunk-rail-trip');
 
-var json = JSON.parse(helper.readFixture('hipmunk_dtw_sfo.json'));
+var json = JSON.parse(helper.readFixture('hipmunk_dtw_sfo.json')),
+    routing = jsonpath.eval(json, '$..routings[?(@.id=="r-0-7-dcaDnycOmay05Djun13")]')[0];
 
 vows.describe('HipmunkLeg').addBatch({
   '.parse': {
     'parses a rail trip if the carrier is Amtrak and route number is < 3000': function() {
-      var legs = HipmunkLeg.parse(json, 'r-0-7-dcaDnycOmay05Djun13');
+      var legs = HipmunkLeg.parse(routing, 'r-0-7-dcaDnycOmay05Djun13');
       assert.instanceOf(legs[0], HipmunkRailTrip);
     },
     'parses a bus trip if the carrier is Amtrak and route number is >= 3000': function(err, $, leg) {
-      var routing = jsonpath.eval(json, '$..routings[?(@.id=="r-0-7-dcaDnycOmay05Djun13")]')[0];
       routing.legs[0].routeNumber = '4578';
-      var searchData = [{ routings: [routing] }];
-      var legs = HipmunkLeg.parse(searchData, 'r-0-7-dcaDnycOmay05Djun13');
+      var legs = HipmunkLeg.parse(routing, 'r-0-7-dcaDnycOmay05Djun13');
       assert.instanceOf(legs[0], HipmunkBusTrip);
     },
     'parses a flight if the carrier is not Amtrak': function(err, $, leg) {
-      var legs = HipmunkLeg.parse(json, 'r-0-0-dtwDsfoOmay05Djun13');
+      var legs = HipmunkLeg.parse(routing, 'r-0-0-dtwDsfoOmay05Djun13');
       assert.instanceOf(legs[0], HipmunkFlight);
       assert.instanceOf(legs[1], HipmunkFlight);
     }
--- test/drivers/hipmunk/hipmunk-trip-test.js
+++ test/drivers/hipmunk/hipmunk-trip-test.js
@@ -1,23 +1,25 @@
 var helper = require('../../helper'),
     vows = helper.vows,
     assert = helper.assert,
-    sinon = helper.sinon;
+    sinon = helper.sinon,
+    jsonpath = require('dkastner-JSONPath');
 
 var HipmunkTrip = helper.plugin.require('./drivers/hipmunk/hipmunk-trip');
 
 var tripExamples = require('../../trip-examples');
 
-var json = JSON.parse(helper.readFixture('hipmunk_dtw_sfo.json'));
+var json = JSON.parse(helper.readFixture('hipmunk_dtw_sfo.json')),
+    routing = jsonpath.eval(json, '$..routings[?(@.id=="r-0-7-dcaDnycOmay05Djun13")]')[0];
 
 vows.describe('HipmunkTrip').addBatch(
   tripExamples('hipmunk_dtw_sfo.html', function($) {
-    return new HipmunkTrip('r-0-0-dtwDsfoOmay05Djun13', $, $('.routing').get(0), json);
+    return new HipmunkTrip('r-0-0-dtwDsfoOmay05Djun13', $, $('.routing').get(0), routing);
   })
 ).addBatch({
   '#loadFlights': {
     'loads the appropriate flight information': function() {
       //var $ = helper.qweryFixture('hipmunk_dtw_sfo.html');
-      var trip = new HipmunkTrip('r-0-0-dtwDsfoOmay05Djun13', null, null, json);
+      var trip = new HipmunkTrip('r-0-0-dtwDsfoOmay05Djun13', null, null, routing);
       trip.loadFlights(function() {});
 
       assert.equal(trip.flights[0].origin, 'DTW');
