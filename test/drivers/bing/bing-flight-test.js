72f0f0323e95b3796ab146be86f4ce88b0500bb1
Derek Kastner <dkastner@gmail.com>
2012-08-29 16:25:57 -0500

    Fix bing flight for new JSON format

--- test/drivers/bing/bing-flight-test.js
+++ test/drivers/bing/bing-flight-test.js
@@ -18,42 +18,42 @@ vows.describe('BingFlight').addBatch({
 
       assert.equal(flights.length, 4);
 
-      assert.equal(flights[0].airline, 'US');
+      assert.equal(flights[0].airline, 'FL');
       assert.equal(flights[0].origin, 'DTW');
-      assert.equal(flights[0].destination, 'CLT');
+      assert.equal(flights[0].destination, 'ATL');
 
-      assert.equal(flights[1].airline, 'US');
-      assert.equal(flights[1].origin, 'CLT');
+      assert.equal(flights[1].airline, 'FL');
+      assert.equal(flights[1].origin, 'ATL');
       assert.equal(flights[1].destination, 'SFO');
 
-      assert.equal(flights[2].airline, 'US');
+      assert.equal(flights[2].airline, 'FL');
       assert.equal(flights[2].origin, 'SFO');
-      assert.equal(flights[2].destination, 'PHX');
+      assert.equal(flights[2].destination, 'ATL');
 
-      assert.equal(flights[3].airline, 'US');
-      assert.equal(flights[3].origin, 'PHX');
+      assert.equal(flights[3].airline, 'FL');
+      assert.equal(flights[3].origin, 'ATL');
       assert.equal(flights[3].destination, 'DTW');
     },
     'follows referenced legs': function(searchData) {
-      var signature = jsonpath.eval(searchData[0], '$.quotes[?(@.journeyId == "17")].pricing.signature')[0];
+      var signature = jsonpath.eval(searchData[0], '$.quotes[?(@.journeyId == "j_8")].pricing.signature')[0];
       var flights = BingFlight.parse(searchData, signature);
 
       assert.equal(flights.length, 4);
 
-      assert.equal(flights[0].airline, 'US');
+      assert.equal(flights[0].airline, 'FL');
       assert.equal(flights[0].origin, 'DTW');
-      assert.equal(flights[0].destination, 'CLT');
+      assert.equal(flights[0].destination, 'ATL');
 
-      assert.equal(flights[1].airline, 'US');
-      assert.equal(flights[1].origin, 'CLT');
+      assert.equal(flights[1].airline, 'FL');
+      assert.equal(flights[1].origin, 'ATL');
       assert.equal(flights[1].destination, 'SFO');
 
-      assert.equal(flights[2].airline, 'US');
+      assert.equal(flights[2].airline, 'FL');
       assert.equal(flights[2].origin, 'SFO');
-      assert.equal(flights[2].destination, 'PHX');
+      assert.equal(flights[2].destination, 'ATL');
 
-      assert.equal(flights[3].airline, 'US');
-      assert.equal(flights[3].origin, 'PHX');
+      assert.equal(flights[3].airline, 'FL');
+      assert.equal(flights[3].origin, 'ATL');
       assert.equal(flights[3].destination, 'DTW');
     }
   }
