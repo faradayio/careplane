42fa203b10e63645de392043b9d6ce8c82bf95e5
Derek Kastner <dkastner@gmail.com>
2012-09-04 11:48:58 -0500

    Automated orbitz and kayak tests

--- features/kayak.feature
+++ features/kayak.feature
@@ -1,4 +1,3 @@
-@functioning
 Feature: Kayak
   As a Careplane user
   I want to see flight footprints on Kayak.com
@@ -6,7 +5,7 @@ Feature: Kayak
   Background:
     Given I am visiting "www.kayak.com"
 
-  @javascript
+  @javascript 
   Scenario: Basic Kayak search
     When I go to the home page
     And I follow "Flights"
@@ -16,5 +15,17 @@ Feature: Kayak
     And I fill in "return_date" with a return date
     And I press "fdimgbutton"
     And Careplane runs as soon as ".flightresult" is visible
+    Then I should see "Carbon calculations powered by"
+    And I should see carbon footprints
+
+  @chrome @firefox @safari 
+  Scenario: Basic Kayak search
+    When I go to the home page
+    And I follow "Flights"
+    And I fill in "origin" with "DTW"
+    And I fill in "destination" with "SFO"
+    And I fill in "depart_date" with a departure date
+    And I fill in "return_date" with a return date
+    And I press "fdimgbutton"
     Then I should see "Carbon calculations powered by"
     And I should see carbon footprints
--- features/step_definitions/careplane_steps.rb
+++ features/step_definitions/careplane_steps.rb
@@ -19,24 +19,28 @@ When /Careplane runs as soon as "(.+)" is visible/ do |selector|
   end
 end
 
+When %r{I click the Flight Only button} do
+  find('span', :text => 'Flight only').click
+end
+
 When %r/I fill in "([^"]+)" with a departure date$/ do |field|
   departure = 2.days.from_now
-  When "I fill in \"#{field}\" with \"#{departure.strftime('%m/%d/%Y')}\""
+  step "I fill in \"#{field}\" with \"#{departure.strftime('%m/%d/%Y')}\""
 end
 When %r/I fill in "([^"]+)" with a return date$/ do |field|
   back = 12.days.from_now
-  When "I fill in \"#{field}\" with \"#{back.strftime('%m/%d/%Y')}\""
+  step "I fill in \"#{field}\" with \"#{back.strftime('%m/%d/%Y')}\""
 end
 
 When %r/I fill in "([^"]+)" with a departure date within "([^"]+)"/ do |field, context|
   departure = 2.days.from_now
-  When %Q{I fill in "#{field}" with "#{departure.strftime('%m/%d/%Y')}" within "#{context}"}
+  step %Q{I fill in "#{field}" with "#{departure.strftime('%m/%d/%Y')}" within "#{context}"}
 end
 When %r/I fill in "([^"]+)" with a return date within "([^"]+)"/ do |field, context|
   back = 12.days.from_now
-  When %Q{I fill in "#{field}" with "#{back.strftime('%m/%d/%Y')}" within "#{context}"}
+  step %Q{I fill in "#{field}" with "#{back.strftime('%m/%d/%Y')}" within "#{context}"}
 end
 
 Then /I should see carbon footprints/ do
-  Then 'I should see /[\d]+,[\d]+[^\s]* lbs CO2e/'
+  step 'I should see /[\d]+,[\d]+[^\s]* lbs CO2e/'
 end
