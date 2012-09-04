80374b8afd8ff335e60ed00846e13cabdefd0c05
Derek Kastner <dkastner@gmail.com>
2012-09-04 11:43:31 -0500

    Use actual extensions in integration tests

--- features/orbitz.feature
+++ features/orbitz.feature
@@ -8,12 +8,25 @@ Feature: Orbitz
   @javascript
   Scenario: Basic Orbitz search
     When I go to the home page
-    And I choose "Flight" within "#productNavMenu"
+    And I choose "air" within "div.products"
     And I fill in "airOrigin" with "DTW" within "#airbotForm"
     And I fill in "airDestination" with "SFO" within "#airbotForm"
     And I fill in "airStartDate" with a departure date within "#airbotForm"
     And I fill in "airEndDate" with a return date within "#airbotForm"
     And I press "Find Flights" within "#airbotForm"
     And Careplane runs as soon as "#matrix" is visible
+    Then I should see "Emission estimates powered by Brighter Planet"
+    And I should see carbon footprints
+
+
+  @chrome @firefox @safari
+  Scenario: Basic Orbitz search
+    When I go to the home page
+    And I click the Flight Only button
+    And I fill in "ar.rt.leaveSlice.orig.key" with "DTW"
+    And I fill in "ar.rt.leaveSlice.dest.key" with "SFO"
+    And I fill in "ar.rt.leaveSlice.date" with a departure date
+    And I fill in "ar.rt.returnSlice.date" with a return date
+    And I press "Search Flights"
     Then I should see "Emission estimates powered by Brighter Planet"
     And I should see carbon footprints
--- features/support/env.rb
+++ features/support/env.rb
@@ -3,16 +3,13 @@ Bundler.setup
 
 require 'cucumber/formatter/unicode' # Remove this line if you don't want Cucumber Unicode support
 
-require 'headless'
-
 require 'capybara'
 require 'capybara/cucumber'
 require 'capybara/session'
-require 'capybara-webkit'
 
-ENV['BROWSER'] ||= 'firefox'
-ENV['CAPY'] ||= 'webkit'
-ENV['PROFILE'] ||= Dir.glob('/Users/dkastner/Library/Application Support/Firefox/Profiles/*.Selenium').first ? Dir.glob('/Users/dkastner/Library/Application Support/Firefox/Profiles/*.Selenium').first : 'default'
+#ENV['BROWSER'] ||= 'firefox'
+#ENV['CAPY'] ||= 'webkit'
+#ENV['PROFILE'] ||= Dir.glob('/Users/dkastner/Library/Application Support/Firefox/Profiles/*.Selenium').first ? Dir.glob('/Users/dkastner/Library/Application Support/Firefox/Profiles/*.Selenium').first : 'default'
 
 # Capybara defaults to XPath selectors rather than Webrat's default of CSS3. In
 # order to ease the transition to Capybara we set the default here. If you'd
@@ -21,18 +18,50 @@ ENV['PROFILE'] ||= Dir.glob('/Users/dkastner/Library/Application Support/Firefox
 Capybara.default_selector = :css
 Capybara.run_server = false
 Capybara.default_wait_time = 3600
-Capybara.javascript_driver = :webkit
+
+require 'capybara-webkit'
+Capybara.default_driver = :webkit
 
 Dir.glob('*.html') { |f| FileUtils.rm_f f }
 
 World(Capybara)
 
 Before do |scenario|
-  @scenario = scenario
-  @headless = Headless.new
-  @headless.start
+  if Capybara.current_driver == :webkit
+    require 'headless'
+
+    @scenario = scenario
+    @headless = Headless.new
+    @headless.start
+  end
 end
 
 After do
-  @headless.destroy
+  @headless.destroy if Capybara.current_driver == :webkit
+end
+
+Capybara.register_driver :selenium_chrome do |app|
+  require 'selenium-webdriver'
+  profile = Selenium::WebDriver::Chrome::Profile.new
+  profile.add_extension File.expand_path(%w(.. .. .. google_chrome build careplane.zip).join('/'), __FILE__)
+  Capybara::Selenium::Driver.new app, :browser => :chrome, :profile => profile
+end
+
+Capybara.register_driver :selenium_firefox do |app|
+  require 'selenium-webdriver'
+  profile = Selenium::WebDriver::Firefox::Profile.new
+  profile.add_extension File.expand_path(%w(.. .. .. firefox build careplane.xpi).join('/'), __FILE__)
+  Capybara::Selenium::Driver.new app, :browser => :firefox, :profile => profile
+end
+
+Before("@chrome") do
+  puts 'Building package...'
+  puts `rake google_chrome:package`, 'Done!'
+  Capybara.current_driver = :selenium_chrome
+end
+
+Before("@firefox") do
+  puts 'Building package...'
+  puts `rake firefox:package`, 'Done!'
+  Capybara.current_driver = :selenium_firefox
 end
